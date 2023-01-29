73e4072e7c8710eecc731d9a7282c62182245d24
Ksenia Popova <foogle80@gmail.com>
2023-01-29 07:11:46 +0300

    finale

--- Task/Program.cs
+++ Task/Program.cs
@@ -12,6 +12,43 @@ void ShowArray(string[] array) {
     }
     System.Console.WriteLine();
 }
+string[] CreateArray() {   
+    int count = 0;
+    string value = String.Empty;
+    string[] secondArray = new string[50];
+    System.Console.WriteLine($"Введите 'exit' для завершения ввода");
+    for (count = 0; value != "exit"; count++) {
+        value = Prompt($"Введите {count+1}-ое значение ");
+        if (value != "exit") {
+            secondArray[count] = value;
+        }
+    }
+    string[] array = new string[count-1];
+    for (int i = 0; i < array.Length; i++) {
+         array[i] = secondArray[i];
+    }
+    return array;
+}
+string[] CheckArray(string[] array) {   
+    int count = 0;
+    int index = 0;
+    for (int i = 0; i < array.Length; i++) { 
+        if (array[i].Length <= 3) count++;
+    }
+    string[] finalArray = new string[count];
+    for (int j = 0; j < array.Length; j++) { 
+        if (array[j].Length <= 3) {
+            finalArray[index] = array[j];
+            index++;
+        }
+    }
+    return finalArray;
+}
 
+string[] array = CreateArray();
+System.Console.Write("Полученные данные: ");
+ShowArray(array);
+System.Console.Write("Результат: ");
+ShowArray(CheckArray(array));
 
 
